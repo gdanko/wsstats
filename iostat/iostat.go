698eb560afca9f648bcfdba4beb255dc2ecc4c81
gdanko <Gary_Danko@intuit.com>
2024-08-30 18:17:48 -0700

    Use our own IOCountersStat because I hate camel case

--- iostat/iostat.go
+++ iostat/iostat.go
@@ -1,31 +1,44 @@
 package iostat
 
 import (
-	"github.com/shirou/gopsutil/net"
+	"github.com/shirou/gopsutil/v3/net"
 )
 
-type IOStatData struct {
-	Interface   string  `json:"interface"`
-	BytesRecv   float64 `json:"bytes_recv"`
-	BytesSent   float64 `json:"bytes_sent"`
-	PacketsRecv uint64  `json:"packets_recv"`
-	PacketsSent uint64  `json:"packets_sent"`
+type IOCountersStat struct {
+	Name        string `json:"name"`
+	BytesSent   uint64 `json:"bytes_sent"`
+	BytesRecv   uint64 `json:"bytes_recv"`
+	PacketsSent uint64 `json:"packets_sent"`
+	PacketsRecv uint64 `json:"packets_recv"`
+	ErrorsIn    uint64 `json:"errors_in"`
+	ErrorsOut   uint64 `json:"errors_out"`
+	DroppedIn   uint64 `json:"dropped_in"`
+	DroppedOut  uint64 `json:"dropped_out"`
+	FifoIn      uint64 `json:"fifo_in"`
+	FifoOut     uint64 `json:"fifo_out"`
 }
 
-func GetData() (output []IOStatData, err error) {
+func GetData() (output []*IOCountersStat, err error) {
 	ioCounters, err := net.IOCounters(true)
 	if err != nil {
-		return []IOStatData{}, err
+		return []*IOCountersStat{}, err
 	}
 
-	for _, ifaceBlock := range ioCounters {
-		output = append(output, IOStatData{
-			Interface:   ifaceBlock.Name,
-			BytesSent:   float64(ifaceBlock.BytesSent),
-			BytesRecv:   float64(ifaceBlock.BytesRecv),
-			PacketsSent: uint64(ifaceBlock.PacketsSent),
-			PacketsRecv: uint64(ifaceBlock.PacketsRecv),
+	for _, block := range ioCounters {
+		output = append(output, &IOCountersStat{
+			Name:        block.Name,
+			BytesSent:   block.BytesSent,
+			BytesRecv:   block.BytesRecv,
+			PacketsSent: block.PacketsSent,
+			PacketsRecv: block.PacketsRecv,
+			ErrorsIn:    block.Errin,
+			ErrorsOut:   block.Errout,
+			DroppedIn:   block.Dropin,
+			DroppedOut:  block.Dropout,
+			FifoIn:      block.Fifoin,
+			FifoOut:     block.Fifoout,
 		})
 	}
+
 	return output, nil
 }
